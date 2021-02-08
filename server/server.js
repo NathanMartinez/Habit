d5dd254daa668e967de7884a6d777b9a779e23cc
Nate Martinez <natemartinez@Nates-MacBook-Pro.local>
2021-02-08 07:46:43 -0700

    Fixed issues with accessing the SQL DB. Added a get route to obtain a list of all users on file.

--- server/server.js
+++ server/server.js
@@ -1,31 +1,30 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 const mysql      = require('mysql');
 const connection = mysql.createConnection({
-  socketPath  : '/Applications/MAMP/tmp/mysql/mysql.sock',
-  user     : 'root',
-  password : 'root',
-  database : 'Habit'
+  socketPath  : process.env.SOCKET_PATH,
+  user        : process.env.USER_NAME,
+  password    : process.env.PASSWORD,
+  database    : process.env.DATABASE
 });
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log('Now listening on port: ' + port);
 })
- 
-// connection.connect();
-//  // Get all users
-// connection.query('SELECT * from users', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log(results);
-// });
 
-// // Add new user
-// connection.query(`INSERT INTO users (firstname, lastname, email, username, passcode)
-// VALUES ('Nate', 'Martinez', 'natedzmtz@yahoo.com', 'NateDzMtz', '1234')`, function (error, results, fields) {
-//   if (error) throw error;
-//   console.log(results, fields);
-// });
-// connection.end();
+app.get('/users', (req, res) => {
+connection.connect();
+connection.query('SELECT * from users', function (error, results, fields) {
+  if (error) throw error;
+  res.send(results);
+});
+connection.end();
+})
